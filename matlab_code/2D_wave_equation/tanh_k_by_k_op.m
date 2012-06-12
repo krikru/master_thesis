f4b1c388d9820ad505a3d3c0a46106ebc5f173cb
krikr808 <krikr808@student.liu.se>
2012-06-12 17:47:11 +0200

    Hacking Octave (trying to create plots and print them to files, not going too well yet.)

--- matlab_code/2D_wave_equation/tanh_k_by_k_op.m
+++ matlab_code/2D_wave_equation/tanh_k_by_k_op.m
@@ -1,14 +1,22 @@
 function conv = tanh_k_by_k_op()
 	tol = 1.0e-6; % Default (1.0e-6)	
 	fun = @(r) 1./(8*pi^3*r.*(1+r)) .* e.^(-r.^2/4);
-	%fun = @(r) r<1;
 	cut_off = 20;
-	res = 0.01;
-	r_vec = [0 : res : 20];
+	res = 0.1;
+	r_vec = [0 : res : 25];
 	ref_fun = @(k) (!k) + (!!k).*tanh(k)./k/(2*pi)^2;
 	F = (2*pi) * hankel_transform(0, fun, cut_off, r_vec, tol);
 	F_ref = ref_fun(r_vec);
-	plot(r_vec, F, r_vec, F_ref);
-	%plot(r_vec, F_ref - F);
-	%plot(r_vec, F_ref ./ F);
+	
+	% Compare functions
+	h = plot(r_vec, F_ref, r_vec, F);
+	print("C:/Work/master_thesis/matlab_code/2D_wave_equation/compare.tex", "-dtex");
+	print("C:/Work/master_thesis/matlab_code/2D_wave_equation/compare_tikz.tex", "-dtikz");
+	%h = plot(r_vec, F_ref, r_vec, F);
+	%print("C:/Work/master_thesis/matlab_code/2D_wave_equation/compare_logarithmic.tex", "-dtex");
+	%print("C:/Work/master_thesis/matlab_code/2D_wave_equation/compare_logarithmic_tikz.tex", "-dtikz");
+	% Ratio
+	h = plot(r_vec, F ./ F_ref);
+	print("C:/Work/master_thesis/matlab_code/2D_wave_equation/ratio.tex", "-dtex");
+	print("C:/Work/master_thesis/matlab_code/2D_wave_equation/ratio_tikz.tex", "-dtikz");
 endfunction
