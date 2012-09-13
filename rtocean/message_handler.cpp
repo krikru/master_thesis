8e6beb3f7587555ef122946620711195e3cbafdc
krikr808 <krikr808@student.liu.se>
2012-09-13 18:50:26 +0200

    Updated the message handler with QStrings instead of std::strings and using qCritical() instead of std::cerr

--- rtocean/message_handler.cpp
+++ rtocean/message_handler.cpp
@@ -5,9 +5,11 @@
 //#define WIN32_LEAN_AND_MEAN
 
 /* Standard includes */
-#include <iostream>
-//#include <windows.h>
 #include <typeinfo>
+
+/* Qt includes */
+#include <QDebug>
+#include <QString>
 
 /* Own includes */
 #include "message_handler.h"
@@ -16,31 +18,39 @@
 // PUBLIC METHODS
 ////////////////////////////////////////////////////////////////
 
-void message_handler::display_message_box(std::string message, std::string caption, QMessageBox::Icon icon)
+void message_handler::display_message_box(QString message, QString caption, QMessageBox::Icon icon)
 {
     QMessageBox mbox;
-    mbox.setText(QString::fromStdString(message));
-    mbox.setWindowTitle(QString::fromStdString(caption));
+    mbox.setText(message);
+    mbox.setWindowTitle(caption);
     mbox.setIcon(icon);
     mbox.exec();
-    //MessageBoxA(0, message.c_str(), caption.c_str(), message_type);
 }
 
-void message_handler::display_warning(std::string message, std::string caption)
+void message_handler::display_information_message(QString message, QString caption)
+{
+    display_message_box(message, caption, QMessageBox::Information);
+}
+
+void message_handler::display_warning_message(QString message, QString caption)
 {
     display_message_box(message, caption, QMessageBox::Warning);
 }
 
-void message_handler::display_error_message(std::string message, std::string caption)
+void message_handler::display_error_message(QString message, QString caption)
 {
     display_message_box(message, caption, QMessageBox::Critical);
 }
 
-void message_handler::inform_about_exception(std::string caller, std::exception &error, bool terminate)
+void message_handler::inform_about_exception(QString caller, std::exception &error, bool terminate)
 {
-    std::cerr << "Unexpected error occured in " << caller << std::endl;
-    std::cerr << "\tCaught: " << error.what() << std::endl;
-    std::cerr << "\tType: " << typeid(error).name() << std::endl;
+    qCritical() << "Unexpected error occured in" << caller;
+    qCritical() << "\tCaught:" << error.what();
+    qCritical() << "\tType:" << typeid(error).name();
+
+    //std::cerr << "Unexpected error occured in " << caller.toStdString() << std::endl;
+    //std::cerr << "\tCaught: " << error.what() << std::endl;
+    //std::cerr << "\tType: " << typeid(error).name() << std::endl;
 #if  0
     display_error_message("Unexpected error occured in " + caller + "\nType: " + typeid(error).name() + "\n\n\"" + error.what() + "\"");
 #endif
--- rtocean/message_handler.h
+++ rtocean/message_handler.h
@@ -6,7 +6,6 @@
 ////////////////////////////////////////////////////////////////
 
 /* Standard includes */
-#include <string>
 #include <stdexcept>
 
 /* Qt includes */
@@ -22,11 +21,12 @@
 
 class message_handler {
 public:
-    static void display_message_box(std::string message, std::string caption = "rtocean",
+    static void display_message_box(QString message, QString caption = "rtocean",
         QMessageBox::Icon icon = QMessageBox::Information);
-    static void display_warning(std::string message, std::string caption = "rtocean");
-    static void display_error_message(std::string message, std::string caption = "rtocean");
-    static void inform_about_exception(std::string caller, std::exception &error, bool terminate = true);
+    static void display_information_message(QString message, QString caption = "rtocean");
+    static void display_warning_message(QString message, QString caption = "rtocean");
+    static void display_error_message(QString message, QString caption = "rtocean");
+    static void inform_about_exception(QString caller, std::exception &error, bool terminate = true);
 };
 
 #endif  /* MESSAGE_HANDLER_H */
